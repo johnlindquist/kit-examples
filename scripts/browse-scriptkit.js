b6f20acbf1a9c501bc260c06d15221dda262052c
John Lindquist <johnlindquist@gmail.com>
2021-08-20 12:02:47 -0600

    removing browse-scriptkit

--- scripts/browse-scriptkit.js
+++
@@ -1,8 +0,0 @@
-// Menu: Browse scriptkit.com
-// Description: Launch scriptkit.com in your browser
-// Author: John Lindquist
-// Twitter: @johnlindquist
-
-// Note: Feel free to delete this script!
-
-exec(`open https://scriptkit.com`)
