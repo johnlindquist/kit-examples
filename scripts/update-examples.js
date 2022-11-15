f8c50a02206801b529d11a9fa49e1a77cc78894a
John Lindquist <johnlindquist@gmail.com>
2022-11-15 14:00:48 -0700

    fix(update): script to update examples

---
+++ scripts/update-examples.js
@@ -0,0 +1,18 @@
+/*
+# Update to the Latest Script Kit Examples
+
+- cd's to the examples directory
+- pulls the latest examples from the script kit repo
+- goes back to the main menu
+*/
+
+// Name: Update Examples
+// Description: Update to the Latest Script Kit Examples
+
+let examplesDir = kenvPath("kenvs", "examples")
+cd(examplesDir)
+
+await $`git stash`
+await $`git pull`
+
+await mainScript()
