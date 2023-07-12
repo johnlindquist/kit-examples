87aec4f8a3703da42bd88e186b3fcf8ad6c0db0e
John Lindquist <johnlindquist@gmail.com>
2023-07-12 12:51:59 -0600

    pushed from Script Kit

---
+++ scripts/scrape-images.js
@@ -0,0 +1,25 @@
+/*
+# Scrape Images Script
+
+- You will be prompted to input an "Image search" query.
+- The script autonomously performs an image search on Google for your input.
+- The script extracts the image source links (`.src`) from the search results and filters to base64
+- An array of these images is then mapped to be displayed as an HTML string.
+- The HTML string is then displayed in a `div` prompt
+*/
+
+// Name: Scrape Images
+// Description: Scrape Images from Google
+
+import "@johnlindquist/kit"
+
+let query = await mini("Image search")
+let srcs = await scrapeSelector(`https://www.google.com/search?q=${query}&tbm=isch`, "img", el => el.src)
+let base64s = srcs.filter(src => src.includes("base64"))
+
+let html = `<div class="flex flex-wrap w-full">
+${base64s.map(src => `<img class="w-24 h-24 object-cover" src="${src}" />`).join("\n")}
+</div>
+`
+
+await div(html)
